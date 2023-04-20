c41418d01307f90d66953316bf617157e1e2ab91
jtjemzz09 <jtjemzz09@LAPTOP-292I2KQV>
2023-04-19 22:06:12 -0700

    made changes

--- greater-than-5.js
+++ greater-than-5.js
@@ -1,8 +1,8 @@
 const num1=5;
-const num2=5;
+const num2=6;
 
 //it will print true if num1 and num2 are greater or equal to 5.
-if (num1 && num2 >= 5){
+if (num1 >=5 && num2>=5){
     console.log(true);
 }
 
--- number-line.js
+++ number-line.js
@@ -1,21 +1,21 @@
 const num1=50;
 const num2=51;
 const sum=num1+num2
-//it will print "101 is greater than 100" if the sum is 101.
+//if the sum is 101, it will print "101 is greater than 100" .
 if (sum === 101){ console.log ("101 is greater than 100");
 }
-//it will print "97 is greater than 0" if the sum is 97.
+// if the sum is 97, it will print "97 is greater than 0".
 else if (sum === 97){ console.log ("97 is greater than 0");
 }
-//it will print "101 is greater than 100" id the sum is 101
+//if  the sum is 101 ,it will print "101 is greater than 100" .
 else if (sum === 101){ console.log ("101 is greater than 100");
 }
-//it will print "0 is equal to 0" if the sum is 0
+//if the sum is 0, it will print "0 is equal to 0" .
 else if (sum === 0){ console.log ("0 is equal to 0");
 }
-//it will print "-1000 is a negative number" if the sum is -1000
+//if the sum is -1000 ,it will print "-1000 is a negative number" .
 else if (sum === -1000){ console.log ("-1000 is a negative number");
 }
-//it will print "-5 is a negative number") if the sum is -5
+//if the sum is -5, it will print "-5 is a negative number") .
 else if (sum === -5){ console.log ("-5 is a negative number");
 }
