1d54678c9c7090aa210b378a4d273f29ba139375
jtjemzz09 <jtjemzz09@LAPTOP-292I2KQV>
2023-04-19 20:57:31 -0700

    made some changes to greater-than5.js

--- greater-than-5.js
+++ greater-than-5.js
@@ -1,7 +1,10 @@
 const num1=5;
 const num2=5;
 
+//it will print true if num1 and num2 are greater or equal to 5.
 if (num1 && num2 >= 5){
     console.log(true);
 }
+
+//it will print false if num1 and num2 are less than 5.
 else{ console.log(false);}
